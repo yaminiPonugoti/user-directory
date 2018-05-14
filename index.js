8e437032fcc10a416cacd5d5a7e5d086b8e444f0
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-14 16:14:15 -0400

    Say yeah when the megaButton is clicked

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('it works!')
+const button = document.querySelector('button.greeting')//combining element n class selector
+//only one element on a page with a given ID
+function changeText(){
+    alert('yeah')
+}
+button.addEventListener('click',changeText)
