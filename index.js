73963c8f8e98f9861f15a82ef21632259425fe3f
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-14 16:33:54 -0400

    Change button text when clicked

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const button = document.querySelector('button.greeting')//combining element n class selector
 //only one element on a page with a given ID
-function changeText(){
-    const para =document.querySelector('p.greeting')
-    para.textContent='Hi friend!'
+function changeText(ev){
+    ev.target.textContent = 'Clickity clicky'
+   //const para =document.querySelector('p.greeting')
+   // para.textContent='Hi friend!'
 }
 button.addEventListener('click',changeText)
