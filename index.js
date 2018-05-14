7611f2601a5b21ed763c2acc4a2e574c925360dc
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-14 19:17:15 -0400

    Change heading text when button clicked, add multiple headings and have another button change the second heading

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const button = document.querySelector('button.greeting')//combining element n class selector
+const button = document.querySelector('button.greeting')
+const button2 = document.querySelector('button.otherOne')
+//combining element n class selector
 //only one element on a page with a given ID
-function changeText(ev){
-    ev.target.textContent = 'Clickity clicky'
-   //const para =document.querySelector('p.greeting')
-   // para.textContent='Hi friend!'
+function changeHeadingText(ev){
+    //ev.target.textContent = 'Clickity clicky'//<-to change button text
+   const headingText =document.querySelector('h1.greeting')
+   headingText.textContent='Welcome to the directory!'
 }
-button.addEventListener('click',changeText)
+function changeOtherHeadingText(ev){
+    //ev.target.textContent = 'Clickity clicky'//<-to change button text
+   const otherheadingText =document.querySelector('h2.otherOne')
+   otherheadingText.textContent='I love Javascript!'
+}
+button.addEventListener('click',changeHeadingText)
+button2.addEventListener('click',changeOtherHeadingText)
