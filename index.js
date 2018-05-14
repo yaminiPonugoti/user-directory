36966b3459299b0226565f5f8fb4420ef8bc3022
Yamini Ponugoti <yaminiponugoti@yaminis-mbp.coldspring.marian.edu>
2018-05-14 16:19:43 -0400

    Change paragraph text when button is clicked

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button.greeting')//combining element n class selector
 //only one element on a page with a given ID
 function changeText(){
-    alert('yeah')
+    const para =document.querySelector('p.greeting')
+    para.textContent='Hi friend!'
 }
 button.addEventListener('click',changeText)
